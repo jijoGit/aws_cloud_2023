41ae31814ef2ea3f6a261c57e0e9d971e129e839
Jijo p <jijo2k6+1@gmail.com>
2024-02-02 15:54:58 +0000

    notification again

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 
 import HomeFeedPage from './pages/HomeFeedPage';
+import HomeFeedPage from './pages/NotificationsFeedPage';
+
+
 import UserFeedPage from './pages/UserFeedPage';
 import SignupPage from './pages/SignupPage';
 import SigninPage from './pages/SigninPage';
@@ -19,6 +22,10 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeFeedPage />
+  },
+  {
+    path: "/notifications",
+    element: <NotificationsFeedPage />
   },
   {
     path: "/@:handle",
